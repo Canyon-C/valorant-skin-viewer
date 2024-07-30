23104b76d970a72935d33564c9795e0952ef7801
Canyon <canyoncavicke@gmail.com>
2024-07-30 16:13:11 -0700

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-"use client";
 import { ValorantSVG } from "@/app/ui/landing/valorant-svg";
-
+import { Suspense } from "react";
 export default function Landing() {
   return (
     <main className="m-0 p-0">
-      <ValorantSVG />
+      <Suspense>
+        <ValorantSVG />
+      </Suspense>
     </main>
   );
 }
--- src/app/skin/page.tsx
+++ src/app/skin/page.tsx
@@ -1,20 +1,23 @@
 import { Filters } from "../ui/filters/filters";
 import { LazyRender } from "../utils/intersection-observer";
 import { Input } from "../ui/search/input";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <main className="">
-      <div className="py-5">
-        <Input placeholder="Search" type="search"></Input>
-      </div>
+      <Suspense>
+        <div className="py-5">
+          <Input placeholder="Search" type="search"></Input>
+        </div>
 
-      <div className="py-5 w-full flex justify-center items-center">
-        {" "}
-        <Filters></Filters>
-      </div>
+        <div className="py-5 w-full flex justify-center items-center">
+          {" "}
+          <Filters></Filters>
+        </div>
 
-      <LazyRender></LazyRender>
+        <LazyRender></LazyRender>
+      </Suspense>
     </main>
   );
 }
