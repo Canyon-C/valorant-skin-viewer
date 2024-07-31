b8db01fc26150eea6fb392bd207d3c6232f6e768
Canyon <canyoncavicke@gmail.com>
2024-07-30 18:15:24 -0700

    bug fix

--- src/app/ui/landing/redirect-button.tsx
+++ src/app/ui/landing/redirect-button.tsx
@@ -5,7 +5,7 @@ const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const Redirect = () => {
   return (
-    <div className="navContainer py-3 px-5 w-full border">
+    <div className="navContainer py-3 px-5 w-full">
       <Link href="./skin">
         <button
           className={`${poppins.className} px-6 py-2 bg-[#ff4655] dark:border-white dark:text-white text-white rounded-lg transform hover:-translate-y-1 transition duration-400`}
