b3bd2484b7fd73b594747483abe3c45c90725458
Canyon <canyoncavicke@gmail.com>
2024-08-08 22:20:46 -0700

    datatable update

--- src/app/ui/table/data-table.tsx
+++ src/app/ui/table/data-table.tsx
@@ -38,34 +38,5 @@ export const DataTable = () => {
         </div>
       </div>
     </DataProvider>
-    // <DataProvider>
-    //   <div className="skinInfoContainer flex flex-col gap-5 p-5 max-h-screen items-center">
-    //     <div className="data-table flex justify-start w-[18rem] sm:w-[40rem] md:w-[45rem] lg:w-[60rem] xl:w-[75rem] grow flex-col border borderAccent rounded-3xl">
-    //       <div className="w-full flex justify-around px-5 rounded-md">
-    //         <h1 className="text-white text-xl py-2 ">Levels</h1>
-    //       </div>
-    //       <div className="w-full flex justify-center items-center h-full ">
-    //         <LevelHud />
-    //       </div>
-    //     </div>
-    //     <div className="data-table flex justify-start w-[18rem] sm:w-[40rem] md:w-[45rem] lg:w-[60rem] xl:w-[75rem] grow flex-col border borderAccent rounded-3xl gap-7">
-    //       <div className="w-full flex justify-around px-5 rounded-md">
-    //         <h1 className="text-white text-xl py-2">Chromas</h1>
-    //       </div>
-    //       <div className="w-full flex flex-col md:flex-row justify-center items-center text-white grow gap-10 md:gap-0 lg:gap-10">
-    //         <SkinChromas />
-    //       </div>
-    //     </div>
-    //     <div className="data-table flex justify-start w-[18rem] sm:w-[40rem] md:w-[45rem] lg:w-[60rem] xl:w-[75rem] flex-col border borderAccent rounded-3xl ">
-    //       <div className="w-full flex justify-around px-5 rounded-md">
-    //         <h1 className="text-white text-xl py-5">View</h1>
-    //       </div>
-    //       <div className="w-full flex flex-col justify-around items-center text-white">
-    //         <SkinChromaVideos />
-    //         <SkinLevelVideos />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </DataProvider>
   );
 };
