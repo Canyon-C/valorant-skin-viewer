4ed390f2547ea3398377bd1f13e621f6db0bd351
Canyon <canyoncavicke@gmail.com>
2024-08-08 23:53:29 -0700

    active skin level indication update
    The current active skin level is not indicated in red.

--- src/app/ui/skinDetails/level-gui.tsx
+++ src/app/ui/skinDetails/level-gui.tsx
@@ -11,58 +11,42 @@ export const LevelHud = () => {
   const pathname = usePathname();
   const { replace } = useRouter();
   const params = new URLSearchParams(searchParams);
-
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [currentLevel, setCurrentLevel] = useState<number | null>(null);
 
   useEffect(() => {
     if (dataInstance && dataInstance.data) {
       setUI(dataInstance.data.levelLength());
     }
+    const levelParam = searchParams.get("level");
+    if (levelParam) {
+      setCurrentLevel(parseInt(levelParam, 10));
+    }
   }, [dataInstance]);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     setScrollPosition(window.scrollY);
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
-
-  // useEffect(() => {
-  //   const scroll = () => {
-  //     console.log(scrollPosition);
-
-  //   };
-  //   scroll();
-  // }, [searchParams.size]);
-
   const handleClick = (level: number) => {
-    if (params.has("level")) {
-      params.delete("level");
-      params.append("level", level.toString());
-    } else {
-      params.append("level", level.toString());
-    }
-
+    params.set("level", level.toString());
     replace(`${pathname}?${params.toString()}`);
+    setCurrentLevel(level);
+  };
+
+  const variants = {
+    default: { backgroundColor: "transparent" },
+    hover: { backgroundColor: "#ffffff28" },
+    tap: { backgroundColor: "#000000", scale: 0.98 },
+    active: { backgroundColor: "#ff4655" },
   };
 
   return (
     <ul className="text-white flex md:flex-col items-center justify-around w-full h-full gap-3 py-5 px-5">
       {UI?.map((level, index) => (
         <motion.div
-          whileHover={{
-            backgroundColor: "#ffffff28",
-            transition: { duration: 0.2 },
-          }}
-          whileTap={{
-            backgroundColor: "#000000",
-            transition: { duration: 0.2 },
-            scale: 0.98,
-          }}
-          className="border w-1/2 rounded-full flex justify-center borderAccent hover:cursor-pointer"
+          variants={variants}
+          initial="default"
+          animate={currentLevel === index ? "active" : "default"}
+          whileHover="hover"
+          whileTap="tap"
+          transition={{ duration: 0.2 }}
+          className={`border w-1/2 rounded-full flex justify-center borderAccent hover:cursor-pointer `}
           key={index}
           onClick={() => {
             handleClick(index);
