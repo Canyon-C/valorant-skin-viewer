dfb3b0581e9a3052b7f2e8c4fc6f0fff17e5fccc
Canyon <canyoncavicke@gmail.com>
2024-10-13 15:11:21 -0700

    cloudflare runtime edge

--- src/app/[skinname]/page.tsx
+++ src/app/[skinname]/page.tsx
@@ -1,5 +1,7 @@
 import { DataTable } from "@/app/ui/table/data-table";
 
+export const runtime = "edge";
+
 export default function SkinDetails() {
   return (
     <main className="m-0 p-0 ">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { ValorantSVG } from "@/app/ui/landing/valorant-svg";
 import { Suspense } from "react";
+
+export const runtime = "edge";
+
 export default function Landing() {
   return (
     <main className="m-0 p-0">
