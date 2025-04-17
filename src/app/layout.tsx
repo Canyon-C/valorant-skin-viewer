bef55cd8a58b6319e9c2c4650f729aa3e82f7594
Ryder-C <rydercasazza@gmail.com>
2025-04-17 16:06:12 -0700

    Add github button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import os from "node:os";
+import GitHubBadge from "./ui/github-badge";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>{children}</body>
+      <body className={`${inter.className} bg-black`}>
+        {children}
+        <GitHubBadge />
+      </body>
     </html>
   );
 }
